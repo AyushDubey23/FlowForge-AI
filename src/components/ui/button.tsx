38d8ec69b1cdb836b89715c9247e5788c7637e52
AyushDubey23 <ayushdubey3740@gmail.com>
2026-07-13 19:46:08 +0530

    fix: resolve invalid DOM properties clip-rule/path-rule in loading button SVG

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -70,9 +70,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
               <path
                 className="opacity-75"
                 fill="currentColor"
-                path-rule="evenodd"
+                fillRule="evenodd"
                 d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
             <span>Please wait</span>
